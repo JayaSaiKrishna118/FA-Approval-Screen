24cf842982bf0aed4299f747f9ea2577cd4b5b5b
shahinp4708 <shahinp4708@gmail.com>
2024-06-10 13:44:43 +0530

    updated

--- SHAHIN/src/app/app.component.ts
+++ SHAHIN/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContentComponent } from './content/content.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ContentComponent,FontAwesomeModule],
+  imports: [RouterOutlet,ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
