975259e4a3919e3f6390c9b225e65a092afb4ad4
7Gloria <96525844+7Gloria@users.noreply.github.com>
2024-06-11 17:06:50 +0530

    Create app.routes.ts

---
+++ Gloria/src/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { ApprovalMainComponent } from './approval-main/approval-main.component';
+import { StudentDetailsComponent } from './approval-main/student-details/student-details.component';
+// import path from 'path';
+export const routes: Routes = [
+
+{ path:'', redirectTo: 'approval-main', pathMatch: 'full' },
+     {path: 'approval-main', component: ApprovalMainComponent,
+        children:
+        [
+            {
+                path: ':id', component: StudentDetailsComponent
+            }
+        ]
+     },
+
+];
